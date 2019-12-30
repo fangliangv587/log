8ed32620e85253035b0594eec1e82680385959f2
fangliangv5 <xinxianzhong414@126.com>
2019-12-30 15:24:52 +0800

    xlog 修复

---
+++ liblog/src/main/java/com/cenco/log/XLevel.java
@@ -0,0 +1,16 @@
+package com.cenco.log;
+
+import com.orhanobut.logger.Logger;
+
+/**
+ * Created by Administrator on 2018/5/5.
+ */
+
+public class Level {
+    public static final int VERBOSE = 0;
+    public static final int DEBUG = 1;
+    public static final int INFO = 2;
+    public static final int WARN = 3;
+    public static final int ERROR = 4;
+    public static final int CRASH = 5;
+}
